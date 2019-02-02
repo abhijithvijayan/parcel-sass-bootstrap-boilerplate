593b0028dd0091ebbec592b68f2233eec1dc8824
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-02-02 20:24:59 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+import './sass/main.scss';
+import $ from "jquery";
+
+import imageURL from './assets/trollHuntersArcadia.png';
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
+});
